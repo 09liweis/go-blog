a1e69cdd77029044765b2adf2b912a0405a9d63e
Sam Li <weisen.li@hotmail.com>
2022-11-23 11:20:41 -0500

    route(group): add api route group

--- main.go
+++ main.go
@@ -20,13 +20,6 @@ func main() {
     })
   })
 
-  ginServer.GET("/api/blog/:id", func(context *gin.Context)  {
-    blogId := context.Param("id")
-    context.JSON(http.StatusOK, gin.H{
-      "blogId": blogId,
-    })
-  })
-
   ginServer.NoRoute(func(context *gin.Context) {
     context.HTML(http.StatusNotFound, "404.html",nil)
   })
@@ -36,5 +29,27 @@ func main() {
       "message": "pong",
     })
   })
+
+  apiGroup := ginServer.Group("/api")
+  {
+    apiGroup.GET("/blogs", func(context *gin.Context)  {
+      page := context.Query("page")
+      limit := context.Query("limit")
+      context.JSON(http.StatusOK, gin.H{
+        "page": page,
+        "limit": limit,
+      })
+    })
+
+    apiGroup.GET("/blog/:id", func(context *gin.Context)  {
+      blogId := context.Param("id")
+      context.JSON(http.StatusOK, gin.H{
+        "blogId": blogId,
+      })
+    })
+  }
+
+
+
   ginServer.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
